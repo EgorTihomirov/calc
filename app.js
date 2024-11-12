84e6ee2d4ddad4b2b162f11ce4b56e2dbe982c71
slizen <tegor8201@gmail.com>
2024-11-12 20:52:49 +0300

    new css

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
-let n = '';
-let dspl = document.getElementById('dspl'); 
-let resultEl = document.getElementById('result'); 
+var n = "";
+var dspl = document.getElementById('dspl');
+var resultEl = document.getElementById('result');
+
 
 function addNumber(num) {
     n += num;
     dspl.value = n;
 }
+
 
 function addOperator(op) {
     if (n.length > 0 && !isNaN(n[n.length - 1])) {
@@ -14,12 +16,64 @@ function addOperator(op) {
     }
 }
 
+
 function calculate() {
     try {
-        let result = eval(n);
-        resultEl.innerText = '= ' + result;
-        n = '';
+        var result = eval(n);
+        resultEl.innerText = "= " + result;
+        n = "";
     } catch (err) {
         alert('Невозможно вычислить выражение.');
     }
+}
+
+
+function clearCalc() {
+    n = "";
+    dspl.value = n;
+    resultEl.innerText = "= ";
+}
+
+
+function backspace() {
+    if (n.length > 0) {
+        n = n.slice(0, -1);
+        dspl.value = n;
+    }
+}
+
+function resetCalc() {
+    n = "";
+    dspl.value = n;
+    resultEl.innerText = "= ";
+}
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const displayInput = document.getElementById('dspl');
+
+    function updateDisplay() {
+        displayInput.value = n;
+    }
+
+    updateDisplay(); 
+});
+
+function clearCalc() {
+    n = "";
+    dspl.value = n;
+    resultEl.innerText = "= ";
+}
+
+function backspace() {
+    if (n.length > 0) {
+        n = n.slice(0, -1);
+        dspl.value = n;
+    }
+}
+
+function resetCalc() {
+    n = "";
+    dspl.value = n;
+    resultEl.innerText = "= ";
 }
