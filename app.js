2d54f1269b4ab21cff322a52efe52e9d986b9467
slizen <tegor8201@gmail.com>
2024-11-12 16:20:35 +0300

    add ponel

--- app.js
+++ app.js
@@ -1,24 +1,6 @@
-var op; 
-
+var n = "";
 function func() {
-  var result;
-  var num1 = Number(document.getElementById("num1").value);
-  var num2 = Number(document.getElementById("num2").value);
-  // смотрим, что было в переменной с действием, и действуем исходя из этого
-  switch (op) {
-    case '+':
-      result = num1 + num2;
-      break;
-    case '-':
-      result = num1 - num2;
-      break;
-    case '*':
-      result = num1 * num2;
-      break;
-    case '/':
-      result = num1 / num2;
-      break;
-  }
-
+  var result = eval(n);
   document.getElementById("result").innerHTML = result;
+  n = ""
 }
