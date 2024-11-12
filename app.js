88c455c124bf00fc7c8318874827666d457ad16d
slizen <tegor8201@gmail.com>
2024-11-12 20:10:11 +0300

    see state n

--- app.js
+++ app.js
@@ -1,6 +1,25 @@
-var n = "";
-function func() {
-  var result = eval(n);
-  document.getElementById("result").innerHTML = result;
-  n = ""
+let n = '';
+let dspl = document.getElementById('dspl'); 
+let resultEl = document.getElementById('result'); 
+
+function addNumber(num) {
+    n += num;
+    dspl.value = n;
+}
+
+function addOperator(op) {
+    if (n.length > 0 && !isNaN(n[n.length - 1])) {
+        n += op;
+        dspl.value = n;
+    }
+}
+
+function calculate() {
+    try {
+        let result = eval(n);
+        resultEl.innerText = '= ' + result;
+        n = '';
+    } catch (err) {
+        alert('Невозможно вычислить выражение.');
+    }
 }
